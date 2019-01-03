72e41071e2e9bc9bd7837b776131ddac9b2d5109
webuser <webuser@hadoop215>
2019-01-03 11:33:58 +0800

    update

--- learn_rnn.py
+++ learn_rnn.py
@@ -4,17 +4,19 @@ import numpy as np
 from tensorflow.contrib.seq2seq import *
 from tensorflow.python.layers.core import Dense
 class model():
-	def __init__(self,embedding_dim,target_len,seq_len):
+	def __init__(self,embedding_dim,data_size,rnn_size):
 		#self.mode=mode
-		self.seq_len=seq_len
-		self.max_len=tf.constant(target_len,dtype=tf.int32)
+		max_target_len=4
+		self.rnn_size=rnn_size
+		self.data_size=data_size
+		self.max_len=tf.constant(max_target_len,dtype=tf.int32)
 		self.embedding_dim=embedding_dim
 		self.inputs = tf.placeholder(tf.int32, [None,None])
 		self.encoder_input = tf.placeholder(tf.int32, [None,None])
 		self.target = tf.placeholder(tf.int32, [None,None])
 		self.inputs_len = tf.placeholder(tf.int32, [None,])
 		self.target_len = tf.placeholder(tf.int32, [None,])
-		self.embeddings_var = tf.get_variable("embedding_var", [self.seq_len,self.embedding_dim])
+		self.embeddings_var = tf.get_variable("embedding_var", [self.data_size,self.embedding_dim])
 		#self.embeddings_var = tf.Variable(tf.truncated_normal(shape=[5, 2], stddev=0.1),name='encoder_embedding')
 		self.embedded_gno = tf.nn.embedding_lookup(self.embeddings_var,self.inputs )
 		self.embedded_target = tf.nn.embedding_lookup(self.embeddings_var,self.encoder_input)
@@ -35,13 +37,13 @@ class model():
 		train_op=optimizer.minimize(cost)
 		return  train_op,cost,self.target,masks
 	def encoder(self):
-		cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=10) 
-		outputs, h1 = tf.nn.dynamic_rnn(cell, self.embedded_gno,dtype=tf.float32)
+		cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.rnn_size) 
+		outputs, h1 = tf.nn.dynamic_rnn(cell, self.embedded_gno,sequence_length=self.inputs_len,dtype=tf.float32)
 		return outputs,h1
 	def decoder(self,mode):
-		cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=10)
+		cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.rnn_size)
 		#output_layer = tf.layers.Dense(5,kernel_initializer=tf.truncated_normal_initializer(mean=0.1,stddev=0.1))
-		output_layer = tf.layers.Dense(self.seq_len)
+		output_layer = tf.layers.Dense(self.data_size)
 		if mode=='predict':
 			start_tokens = tf.tile(tf.constant([0],dtype=tf.int32),[3],name='start_token')
 			helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.embeddings_var,start_tokens,3)
@@ -61,11 +63,11 @@ class model():
 		return  out
 	def train(self):
 		#index[0] == 'GO'   ,index[3] == 'EOS'
-		x=[[0,1,2,3],[0,2,1,3],[0,2,1,3]]   
+		x=[[1,1,2,2],[1,2,1,2],[2,2,1,2]]   
 		x_len=[4,4,4]
-		y=[[0,1,2],[0,2,1],[0,2,1]]
-		y_len=[3,3,3]
-		y_target=[[1,2,3],[2,1,3],[2,1,3]]
+		y=[[0,1,2,2],[0,2,1,3],[0,1,2,2]]
+		y_len=[4,3,4]
+		y_target=[[1,2,2,3],[2,1,3,3],[1,2,2,3]]
 		with tf.Session() as sess:
 			tf.get_variable_scope().reuse_variables()
 			sess.run(tf.global_variables_initializer())
@@ -78,8 +80,9 @@ class model():
                 	print('Model Trained and Saved')
 
 	def predict(self):
-                x=[[2,1,2,3],[2,2,1,3],[2,2,1,3]]
+                x=[[1,1,2,3],[1,2,1,3],[2,2,1,3]]
                 x_len=[4,4,4]
+
                 with tf.Session() as sess:
 			checkpoint = "data/trained_model.ckpt"
 			saver=tf.train.Saver()
@@ -95,9 +98,9 @@ class model():
 
 
 if __name__== '__main__':
-	encoder=model(10,3,4)
+	encoder=model(embedding_dim=10,data_size=4,rnn_size=10)
 	encoder.train()
-	encoder.predict()
+#	encoder.predict()
 '''
 inputs = tf.placeholder(tf.float32, [None,4,1])
 cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=10)
